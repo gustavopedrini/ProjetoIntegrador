d58e3a55932995ddfac634ff6376011db5b198e9
djalma <djalmahy@hotmail.com>
2023-11-10 14:13:36 -0300

    Feat: Created component and features template

---
+++ frontend/src/components/loading/Loading.jsx
@@ -0,0 +1,14 @@
+import "./loading.css"
+import loading from "../../assets/loading.png";
+
+
+const Loading = () => {
+    return (
+        <>
+            <img className="loading" src={loading} alt="loading" />
+        </>
+    )
+}
+
+
+export default Loading;
---
+++ frontend/src/pages/Main.jsx
@@ -0,0 +1,15 @@
+import Loading from "../components/loading/Loading";
+
+import "../features/main/main.css";
+
+
+const Main = () => {
+    return (
+        <div className="main">
+            <Loading />
+        </div>
+    );
+};
+
+
+export default Main;
