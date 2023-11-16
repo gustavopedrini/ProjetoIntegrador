9104404e6917e9d0b183a1b35522b9c0b2d51147
djalma <djalmahy@hotmail.com>
2023-11-16 16:42:16 -0300

    Chore: Created requester use example

--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -8,7 +8,7 @@ class UserController {
     async GetUsers(_, res) {
         try {
             const users = await services.GetUsers();
-            res.status(200).json({ data: users });
+            res.status(200).json({ content: users });
         }
         catch (error) {
             res.status(500).json({ message: error.message });
--- frontend/src/data/Requester.js
+++ frontend/src/data/Requester.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseURL = "http://localhost:3000/";
+const baseURL = "http://localhost:3001/";
 
 /**
  * @param {*} method The method which axios will execute "post", "get", "put" or "delete".
