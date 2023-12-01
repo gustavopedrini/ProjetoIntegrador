d193b36693af62eb6ee63fed17c06220ae7cd44e
YH1d3k12 <djalmahy@hotmail.com>
2023-11-30 21:31:33 -0300

    Feat: created home page

---
+++ frontend/src/features/home/components/AsideNavigationMenu.jsx
@@ -0,0 +1,43 @@
+import { Link  } from "react-router-dom";
+
+import user from "../../../assets/images/user.png";
+import home from "../../../assets/images/home.png";
+import profile from "../../../assets/images/profile.png";
+import bell from "../../../assets/images/bell.png";
+import gear from "../../../assets/images/gear.png";
+import "./asideNavigationMenu.css"
+
+
+const AsideNavigationMenu =() => {
+    return (
+        <div className="aside-navigation-menu-container">
+            <div className="anm-user-profile">
+                <div><img src={user} alt="" /></div><span>João Fuckner Pyres</span>
+            </div>
+            <div className="anm-flex-container">
+                <nav className="anm-navigation-links">
+                    <ul>
+                        <li>
+                            <Link to="/home"><img src={home} alt="home" /><span>Início</span></Link>
+                        </li>
+                        <li>
+                            <Link to="/home"><img src={profile} alt="profile" /><span>Políticos</span></Link>
+                        </li>
+                        <li>
+                            <Link to="/home"><img src={bell} alt="bell" /><span>Notificações</span></Link>
+                        </li>
+                        <li>
+                            <Link to="/home"><img src={gear} alt="gear" /><span>Configurações</span></Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="anm-btn-report">
+                    <button>Reportar</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+export default AsideNavigationMenu;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import { Container } from '@mui/material';
+import { Outlet } from "react-router-dom";
+
+import HeaderLogo from "../features/home/components/HeaderLogo.jsx";
+import AsideNavigationMenu from "../features/home/components/AsideNavigationMenu.jsx";
+
+import "../features/home/home.css";
 
 
 const Home = () => {
     return (
-        <Container disableGutters sx={{ bgcolor: "tomato", height: "100vh" }}>HOme</Container>
+        <div className="home-page-content-wrapper">
+            <HeaderLogo/>
+            <div className="home-flex-body">
+                <AsideNavigationMenu/>
+                <Outlet/>
+                <h1>ESTOU NA DIREITA</h1>
+            </div>
+        </div>
     );
 };
 
